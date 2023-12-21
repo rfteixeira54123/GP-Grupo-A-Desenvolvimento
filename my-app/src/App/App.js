6e35b4fd044c84bc85cfab0c272c03954637f237
Samuel Martins <98957529+SamuelMartinsS@users.noreply.github.com>
2023-12-21 13:15:18 +0000

    Testes com Post & Delete

--- my-app/src/App/App.js
+++ my-app/src/App/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
-import GetCandidatosListar from '../Components/GetCandidatos';
+import GetCandidatosListar from "../Components/GetCandidatos";
+import Form from "../Components/FormPost";
 
 function App() {
+  useEffect(() => {
+    if (document) {
+      const stylesheet = document.createElement("link");
+      stylesheet.rel = "stylesheet";
+      stylesheet.href =
+        "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
+
+      document.head.appendChild(stylesheet);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <GetCandidatosListar/>
+        <div className="py-6">
+          <Form />
+        </div>
       </header>
     </div>
   );
