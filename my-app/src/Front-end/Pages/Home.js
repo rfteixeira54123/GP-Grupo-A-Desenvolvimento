76f1260f3b9a4d6e7bf41957a2d77bc05072c9d4
SamuelMartinsS <ssamartins6@gmail.com>
2024-01-24 16:10:16 +0000

    Alterações na forma como o Login se comporta

--- my-app/src/Front-end/Pages/Home.js
+++ my-app/src/Front-end/Pages/Home.js
@@ -1,16 +1,19 @@
-
 import Admin from "./Admin/Base";
 import Eleitor from "./Eleitor/Base";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Login from "./Login";
+const Home = () => {
+  const navigate = useNavigate();
 
-const Home = () =>{
-
-    // Decide se exibe páginas de eleitor (true) ou administrador (false).
+  const handleLogin = () => {
     let option = false;
+    if (!localStorage.getItem("Token")) return <Login />;
+    else if (option) return <Eleitor />;
+    else return <Admin />;
+  };
 
-    if(option)
-        return (<Eleitor />);
-    else
-        return(<Admin />);
+  return handleLogin();
 };
 
 export default Home;
